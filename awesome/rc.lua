116592a60ad5c7539a64e07c6b64836c47c0dda0
rklyz <95261992+rklyz@users.noreply.github.com>
2022-08-01 13:03:51 +0800

    Update rc.lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -1,6 +1,3 @@
--- awesome_mode: api-level=4:screen=on
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
 
 -- Standard awesome library
@@ -11,9 +8,6 @@ local beautiful = require "beautiful"
 require "main.error_handling"
 
 beautiful.init(gears.filesystem.get_configuration_dir() .. "theme/theme.lua")
-
-local naughty = require("naughty")
-local ruled = require("ruled")
 
 -- init config
 require "main.wallpaper"
@@ -28,20 +22,6 @@ require "awful.autofocus"
 
 -- init widget
 require "misc"
-
-ruled.notification.connect_signal('request::rules', function()
-    ruled.notification.append_rule {
-        rule       = { },
-        properties = {
-            screen           = awful.screen.preferred,
-            implicit_timeout = 5,
-        }
-    }
-end)
-
-naughty.connect_signal("request::display", function(n)
-    naughty.layout.box { notification = n }
-end)
 
 -- Autorun at startup
 awful.spawn.with_shell("bash ~/.config/awesome/main/autorun.sh")
